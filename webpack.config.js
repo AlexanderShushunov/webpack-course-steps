6a4ee7c5be9c56270f38f144a36572606dafd70f
Alexandr Shushunov <ashushunov@mail.ru>
2017-08-25 21:15:54 +0300

    Split webpack config by tasks.

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,45 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
 
 const PATHS = {
     source: path.resolve(__dirname, 'src'),
     build: path.resolve(__dirname, 'build')
 };
 
-const common = {
-    entry: {
-        'index': path.resolve(PATHS.source, 'pages', 'index', 'index.js'),
-        'other': path.resolve(PATHS.source, 'pages', 'other', 'other.js')
-    },
-    output: {
-        path: PATHS.build,
-        filename: '[name].js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            chunks: ['index'],
-            template: path.resolve(PATHS.source, 'pages', 'index', 'index.pug')
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'other.html',
-            chunks: ['other'],
-            template: path.resolve(PATHS.source, 'pages', 'other', 'other.pug')
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                options: {
-                    pretty: true
-                }
-            }
+const common = merge(
+    {
+        entry: {
+            'index': path.resolve(PATHS.source, 'pages', 'index', 'index.js'),
+            'other': path.resolve(PATHS.source, 'pages', 'other', 'other.js')
+        },
+        output: {
+            path: PATHS.build,
+            filename: '[name].js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                chunks: ['index'],
+                template: path.resolve(PATHS.source, 'pages', 'index', 'index.pug')
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'other.html',
+                chunks: ['other'],
+                template: path.resolve(PATHS.source, 'pages', 'other', 'other.pug')
+            })
         ]
     },
-};
-
-const development = {
-    devServer: {
-        open: true
-    }
-};
+    pug()
+);
 
 module.exports = function (env) {
     if (env === 'production') {
         return common;
     }
     if (env === 'development') {
-        return Object.assign({}, common, development);
+        return merge(common, devserver());
     }
 };
