9360302185e95c12d4bd9b31fd949e7b6d32e845
Alexandr Shushunov <ashushunov@mail.ru>
2017-08-25 20:53:01 +0300

    Add webpack-dev-server.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,6 @@
 import {createMenu} from '../../components/menu';
 
-let menu = createMenu('Main', 'Other');
+let menu = createMenu('-Main-', 'Other');
 document.body.appendChild(menu);
 
 console.log('in index.js');
